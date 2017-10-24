031295a07116b55c574855545a5f188c40ce7827
farfan730 <farfan730@hotmail.com>
2017-10-24 18:20:18 -0500

    variables para conexion

--- home.aspx.cs
+++ home.aspx.cs
@@ -4,9 +4,15 @@ using System.Linq;
 using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
+using System.Data.SqlClient;
+
 
 public partial class home : System.Web.UI.Page
 {
+    SqlConnection con = new SqlConnection();
+    SqlCommand cmd = new SqlCommand();
+    SqlDataReader dr;
+
     protected void Page_Load(object sender, EventArgs e)
     {
 
