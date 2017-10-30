398a883c0b618628fbca17bf52ab698244df3042
farfan730 <farfan730@hotmail.com>
2017-10-30 10:14:54 -0500

    terminañdo

--- home.aspx.cs
+++ home.aspx.cs
@@ -1,12 +1,14 @@
 ﻿using System;
-using System.Data.SqlClient;
-using System.Windows.Forms;
+using System.Data.SqlClient;   //se agregan para la conexion a bd
+using System.Windows.Forms;   //se agrega para la ventana emergente
 
 
 public partial class home : System.Web.UI.Page
 {
     SqlConnection con = new SqlConnection("Data Source=(localdb)\\MSSQLLocalDB;Initial Catalog=C:\\USERS\\JULIAN\\DOCUMENTS\\CLIENTES.MDF;Integrated Security=True;Connect Timeout=30;Encrypt=False;TrustServerCertificate=False;ApplicationIntent=ReadWrite;MultiSubnetFailover=False");
     SqlCommand cmd = new SqlCommand();
+
+    //para las listas
     SqlDataReader dr;
 
     protected void Page_Load(object sender, EventArgs e)
@@ -21,5 +23,22 @@ public partial class home : System.Web.UI.Page
         cmd.ExecuteNonQuery();
         cmd.Clone();
         MessageBox.Show("cliente agregado!!!", " Gracias");
+        TextBox1.Text = "";
+        TextBox2.Text = "";
+        TextBox3.Text = "";
+
+        //desde aqui empezamos a llamar metos
+        
     }
+
+
+
+    public void cargar() {
+
+    }
+
+
+
+
+
 }
